8b1eaed43af4513ac71395a6d73043429e229c72
djo kangoma <djokangoma@gmail.com>
2023-12-11 13:32:27 +0100

    add modification

--- src/components/trend-squid.jsx
+++ src/components/trend-squid.jsx
@@ -4,7 +4,8 @@ export default function TrendSquid() {
   return (
     <div className="trendsquid">
       <p className="para-trendsquid">
-        tranding in Turkey <img src="src/icons/More-2.png" alt="icon-More-2" />
+        tranding in Turkey{" "}
+        <img src="src/icons/More-2.png" alt="icon-More-2 " className="img10" />
       </p>
       <h5>#SQUIDS</h5>
       <p>2,066 Tweets</p>
