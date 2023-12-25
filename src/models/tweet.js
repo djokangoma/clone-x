e84609a93f0b6205ee8776fec406f2230e67de81
djo kangoma <djokangoma@gmail.com>
2023-12-25 01:20:50 +0100

    create page js

---
+++ src/models/tweet.js
@@ -0,0 +1,10 @@
+import {}
+const tweets = [
+  {
+    id: "1",
+    Text1: "CNN",
+    span: "@CNN 7m",
+    Text2:
+      'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.',
+  },
+];
