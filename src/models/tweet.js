2d0436503dc4feb0b2094645d021c3054fe7dd3d
djo kangoma <djokangoma@gmail.com>
2023-12-27 14:19:58 +0100

    more modified

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -1,31 +1,29 @@
-import {Nom delavariable1 as Nom de l'image} from "chemin de l'image"
+//  import {Nom delavariable1 as Nom de l'image} from "chemin de l'image"
 
-import {Nom delavariable2 as Nom de l'image} from "chemin de l'image"
+// import {Nom delavariable2 as Nom de l'image} from "chemin de l'image"
 
-import {Nomdelavariable3 as Nom del'image} from "chemin de l'image"
+// import {Nomdelavariable3 as Nom del'image} from "chemin de l'image"
 
- tweetData.js
+//  tweetData.js
 
-import [nom de la variable1, nom de la variable 2, nom de la variable3] from "chemin de Img-service.js"
+// import [nom de la variable1, nom de la variable 2, nom de la variable3] from "chemin de Img-service.js"
 
-const tweetData = [
-     {
-       id: 1,
-       avatar: nom de la variable,
-       tweetText: "Tu écrit ton text"
-    },
-     {
-       id: 2,
-       avatar: nom de la variable,
-       tweetText: "Tu écrit ton text"
-    },
-    {
-       id: 3,
-       avatar: nom de la variable,
-       tweetText: "Tu écrit ton text"
-    },
-]
+// const tweetData = [
+//      {
+//        id: 1,
+//        avatar: nom de la variable,
+//        tweetText: "Tu écrit ton text"
+//     },
+//      {
+//        id: 2,
+//        avatar: nom de la variable,
+//        tweetText: "Tu écrit ton text"
+//     },
+//     {
+//        id: 3,
+//        avatar: nom de la variable,
+//        tweetText: "Tu écrit ton text"
+//     },
+// ]
 
-
-export default tweetData;
-
+// export default tweetData;
