282eea892c383a5a50e0933d01f28a58050e10a0
djo kangoma <djokangoma@gmail.com>
2024-01-12 15:31:40 +0100

    User profile page where there is their information

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import Home from "./home";
+import Avatar from "../components/avatar";
 
 function Profile() {
   return (
-    <>
-      <Home />
-    </>
+    <div className="userProfile">
+      <h1>Bradley Ortiz</h1>
+      <p> 0 post </p>
+      <div className="userProfileAvatar">
+        <Avatar />
+      </div>
+      <button className="btnUserProfile"> Edit profile</button>
+      <h1>Bradley Ortiz</h1>
+      <p>@Bradley Ortiz</p>
+    </div>
   );
 }
 export default Profile;
