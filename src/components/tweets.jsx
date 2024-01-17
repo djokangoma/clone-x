dc99c383bf2f12b05dc6aa0c875d8eed9eaf1a2d
djo kangoma <djokangoma@gmail.com>
2024-01-17 23:53:29 +0100

    add some data in the component tweetData.js

--- src/components/tweets.jsx
+++ src/components/tweets.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import tweetData from "../modelss/tweetData";
+import tweetData from "../models/tweetData";
 import Tweet from "./tweet";
 
 export default function Tweets() {
